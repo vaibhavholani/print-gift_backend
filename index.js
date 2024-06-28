eebfd7d118801d2a87fb9874f4e8588c993fa6c4
Vaibhav Holani <vaibhavholani@gmail.con>
2024-06-28 16:38:28 -0400

    checking vercel logging

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from "express";
 const app = express();
+app.use(express.json())
+
+console.log("I am alive");
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+// Add a POST route at /webhook/orders/create
+// This route will be used to receive the order data from Shopify
+app.post("/webhook/orders/create", (req, res) => {
+
+//   console.log(req)
+  console.log(req.body);
+  res.send("Order received");
+});
+
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
