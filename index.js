e15e10b7bdb7cfa84696cac03747cf0262d2d01e
Vaibhav Holani <vaibhavholani@gmail.con>
2024-06-28 16:46:11 -0400

    correcting webhook path

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from "express";
 const app = express();
 app.use(express.json())
 
-console.log("I am alive");
+
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Add a POST route at /webhook/orders/create
 // This route will be used to receive the order data from Shopify
-app.post("/webhook/orders/create", (req, res) => {
+app.post("/webhooks/orders/create", (req, res) => {
 
 //   console.log(req)
   console.log(req.body);
